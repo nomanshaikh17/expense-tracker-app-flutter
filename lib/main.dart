e3eaa39c7b70c08f1cbc0c7501a63b38f1ac2aea
Noman Shaikh <noman.shaikh49@yahoo.com>
2020-06-30 15:04:25 +0500

    completed till lecture # 94

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,108 @@
+import 'package:expensetracker/widgets/new_tranaction.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import './widgets/user_transactions.dart';
+import './widgets/transactions_list.dart';
+import './models/transactions.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Appbar"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () {},
+      title: 'My Expense Tracker',
+      home: MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+        fontFamily: 'QuickSand',
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transactions> transaction = [
+    Transactions(
+      id: 't1',
+      title: 'fun',
+      amount: 50.5,
+      date: DateTime.now(),
+    ),
+    Transactions(
+      id: 't2',
+      title: 'fuel',
+      amount: 98.0,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void addNewTransaction(String title, double amount) {
+    final newTx = Transactions(
+      amount: amount,
+      title: title,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+    setState(() {
+      transaction.add(newTx);
+    });
+  }
+
+  void startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Expense Tracker"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => startAddNewTransaction(context),
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text("chart"),
+              color: Colors.blue,
+              elevation: 10,
             ),
-          ],
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Text("chart"),
-                color: Colors.blue,
-                elevation: 10,
-              ),
-            ),
-            UserTransactions(),
-          ],
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
+          ),
+          TransactionList(
+            transaction: transaction,
+          )
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => startAddNewTransaction(context),
       ),
     );
   }
--- lib/widgets/new_tranaction.dart
+++ lib/widgets/new_tranaction.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
-
+class NewTransaction extends StatefulWidget {
   final Function addNewTransaction;
 
   NewTransaction(this.addNewTransaction);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   void onSubmit() {
     final title = titleController.text;
@@ -15,10 +21,11 @@ class NewTransaction extends StatelessWidget {
     if (title.isEmpty || amount <= 0) {
       return;
     }
-    addNewTransaction(
+    widget.addNewTransaction(
       title,
       amount,
     );
+    Navigator.of(context).pop();
   }
 
   @override
